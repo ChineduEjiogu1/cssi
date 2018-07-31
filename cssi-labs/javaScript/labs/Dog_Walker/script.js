333dcbf29d5773841fae34ce430218775b4141a0
ChineduEjiogu1 <chineduejiogu@gmail.com>
2018-07-31 09:43:05 -0400

    Pre Javascript quiz

--- cssi-labs/javaScript/labs/Dog_Walker/script.js
+++ cssi-labs/javaScript/labs/Dog_Walker/script.js
@@ -15,15 +15,19 @@
 // Task 1
 let dogName1 = "Steve";
 let dogType1 = "beagle";
-
+ console.log ("I will walk " + dogName1 + " today at 12:00pm" );
 // Complete Task 1 Below
 
 
 
 let dogName2 = "Joe";
-let dogType2 = "bulldog";
+let dogType2 = "corgi";
 
 // Complete Task 2 Below
+if(dogType2 === "corgi")
+{
+  console.log ("I will walk " + dogName2 + " today at 12:00pm");
+}
 
 
 
@@ -31,3 +35,18 @@ let dogName = "Lola";
 let dogType = "poodle";
 
 // Complete Task 3 Below
+
+if(dogType === "beagle" || "corgi")
+{
+  console.log("I will walked " + dogName1 + " or " + dogName2 +  " today at 12:00pm");
+
+}
+else if (dogType === "bulldog")
+{
+  console.log("I will walk " + dogName + " today at 1:00pm");
+}
+
+else
+{
+  console.log("I will walk " + dogName + " today at 2:00pm");
+}
--- lab-JS1/app.js
+++ lab-JS1/app.js
@@ -1,4 +1,15 @@
 function sendAlert()
 {
-  alert("Welcome to CSSI 2018");
+  //alert("Welcome to CSSI 2018");
+  console.log("Hello World!");
+var name1 = "Bob";
+let name2 = "Bill";
+const name3 = "Bo";
+
+name1 = "Amy";
+name2 = "Alice";
+//name3 = "Ari";
+console.log (name1);
+console.log (name2);
+console.log (name3);
 }
