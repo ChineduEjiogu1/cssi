2c0383defb5a6103636b8755b65c80879372ec81
ChineduEjiogu1 <chineduejiogu@gmail.com>
2018-07-31 16:37:41 -0400

    alarm clock function

--- cssi-labs/javaScript/labs/Alarm_Clock/script.js
+++ cssi-labs/javaScript/labs/Alarm_Clock/script.js
@@ -1,4 +1,4 @@
-// Copyright 2018 Google LLC
+-+// Copyright 2018 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,3 +13,51 @@
 // limitations under the License.
 
 console.log("script is running...");
+
+//1
+function Basic_Alarm(alarmMessage)
+{
+  return alarmMessage;
+}
+
+
+console.log(Basic_Alarm("My alarm"));
+
+function My_Alarm(alarmMessage)
+{
+  return alarmMessage;
+}
+
+console.log(My_Alarm("Hey, Chinedu, wake up it's 7:00am"));
+
+//2
+function Mom_Alarm(alarmMessage)
+{
+  return alarmMessage;
+}
+console.log(Mom_Alarm("Hey, Mom, wake up! it's 9:00pm"));
+
+//3
+function Family_Alarm(familyMem, time)
+{
+  return "Hey, " + familyMem + " , wake up! It’s " +  time;
+}
+
+console.log(Family_Alarm("Bill", "7am"));
+
+//4
+function important_Alarm(alarmMessage)
+{
+  return alarmMessage;
+}
+
+console.log(important_Alarm("WAKE UP, WAKE UP, WAKE UP!!"));
+
+//5
+function Snooze_Alarm(oriTime)
+{
+  let newTime = oriTime + 1
+  return "The alarm for " + oriTime + " has changed to " + newTime;
+}
+
+console.log(Snooze_Alarm (3));
