22870bac376d8ad73329025d6466af74ad5ee491
ChineduEjiogu1 <chineduejiogu@gmail.com>
2018-08-06 20:36:04 -0400

    second and a half commit

--- myProject1/script.js
+++ myProject1/script.js
@@ -1,3 +1,7 @@
+const selectedTiles = [];
+let tilesIds = [];
+const patternTiles = [];
+
 let playingTile = document.querySelectorAll(".playingtile");
 
 playingTile.forEach((e)=>{
@@ -5,11 +9,30 @@ playingTile.forEach((e)=>{
 });
 
 function colorChange(e){
-    e.target.classList.toggle("colorOnClick")
+    e.target.classList.add("colorOnClick")
+    selectedTiles.push(this.id);
 };
 
 
 function randGene()
 {
+    for(let i = 1; i<=25; i++)
+    {
+        tilesIds.push('playTile' + i);
+    }
+
+    let patternLength = Math.floor(Math.random()*3)+7;
+
+    for(let c = 0, item = 0,i = 0; c < patternLength; c++)
+    {   
+        i = Math.floor(Math.random()*tilesIds.length);
+        item = tilesIds[i];
+        tilesIds.splice(i,1);
+        patternTiles.push(item);
+    }
+
+    patternTiles.forEach((e)=>{
+        document.getElementById(e).classList.add('colorOnClick');
+    })
 
 }
