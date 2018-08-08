1b5336cd27677ec571ff5420f00872528a418545
ChineduEjiogu1 <chineduejiogu@gmail.com>
2018-08-08 16:08:30 -0400

    first commit

---
+++ week3/Labs-py/MadLibs/cardio function.py
@@ -0,0 +1,33 @@
+'''
+name = str(raw_input("Enter a name: "))
+word = str(raw_input("Enter a noun: "))
+adjective = str(raw_input("Enter a adjective: "))
+
+def longest_word(name.word.adjective):
+
+    if(name > word and name > adjective):
+        return name
+        print(name)
+
+    elif(word > name and word > adjective)
+        return word
+        print(word)
+
+    elif(adjective > word and adjective > name)
+        return adjective
+        print (adjective)
+
+'''
+aWord = "Hello!" # "olleH"
+def reverse_string(aWord):
+    s = ""
+    for i in range(len(aWord),0,-1):
+        s = s + aWord[i-1]
+    print s
+
+
+reverse_string(aWord)
+
+
+def sum_to_n(int):
+    
