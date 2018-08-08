d2e1e66c28fba782c5f078e573fd4eef9eb86218
ChineduEjiogu1 <chineduejiogu@gmail.com>
2018-08-08 16:23:00 -0400

    fifth commit

--- myProject1/script.js
+++ myProject1/script.js
@@ -14,28 +14,53 @@ function colorChange(e){
 };
 
 
-function randGene()
-{
+// function randGene()
+// {
+//     for(let i = 1; i<=25; i++)
+//     {
+//         tilesIds.push('playTile' + i);
+//     }
+
+//     let patternLength = Math.floor(Math.random()*3)+7;
+
+//     for(let c = 0, item = 0,i = 0; c < patternLength; c++)
+//     {   
+//         i = Math.floor(Math.random()*tilesIds.length);
+//         item = tilesIds[i];
+//         tilesIds.splice(i,1);
+//         patternTiles.push(item);
+//     }
+
+//     patternTiles.forEach((e)=>{
+//         document.getElementById(e).classList.add('colorOnClick');
+//     })
+
+// }
+function timer(ms) {
+    return new Promise(res => setTimeout(res, ms));
+ }
+  
+ async function randGene()
+ {
     for(let i = 1; i<=25; i++)
     {
         tilesIds.push('playTile' + i);
     }
-
+ 
     let patternLength = Math.floor(Math.random()*3)+7;
-
+ 
     for(let c = 0, item = 0,i = 0; c < patternLength; c++)
-    {   
+    {  
         i = Math.floor(Math.random()*tilesIds.length);
         item = tilesIds[i];
         tilesIds.splice(i,1);
         patternTiles.push(item);
+        console.log(c);
+        document.getElementById(item).classList.add('colorOnClick');
+        await timer(1500);
     }
-
-    patternTiles.forEach((e)=>{
-        document.getElementById(e).classList.add('colorOnClick');
-    })
-
-}
+ 
+ }
 // Timer for the game to start
 setTimeout(randGene, 5000);
 
